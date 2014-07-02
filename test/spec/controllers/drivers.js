67f93f1be254174bcf4018bf3a33b7a66ea9d36d
Khemraj Thapa <khyamay@gmail.com>
2014-07-03 00:09:06 +0100

    fixed typos

--- test/spec/controllers/drivers.js
+++ test/spec/controllers/drivers.js
@@ -1,74 +1,74 @@
-'use strict';
+
 
 describe('Controller: driversCtrl', function () {
 
   // load the controller's module
   beforeEach(module('f1App'));
 
-//   var driversCtrl, scope;
+  var driversCtrl, scope;
 
-//   // Initialize the controller and a mock scope
-//   beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
-//     var scope = $rootScope.$new();
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope, $httpBackend) {
+    scope = $rootScope.$new();
     
-//     var httpMock = $httpBackend;
+    httpMock = $httpBackend;
 
-//   httpMock.expectJSONP("http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK").respond(
-//       {"MRData": {"StandingsTable": {"StandingsLists" : [{"DriverStandings":[
-//         {
-//           "Driver": {
-//               "givenName": 'Sebastian',
-//               "familyName": 'Vettel'
-//           },
-//           "points": "397",
-//           "nationality": "German",
-//           "Constructors": [
-//               {"name": "Red Bull"}
-//           ]
-//         },
-//         {
-//           "Driver": {
-//               "givenName": 'Fernando',
-//               "familyName": 'Alonso'
-//           },
-//           "points": "242",
-//           "nationality": "Spanish",
-//           "Constructors": [
-//               {"name": "Ferrari"}
-//           ]
-//         },
-//         {
-//           "Driver": {
-//               "givenName": 'Mark',
-//               "familyName": 'Webber'
-//           },
-//           "points": "199",
-//           "nationality": "Australian",
-//           "Constructors": [
-//               {"name": "Red Bull"}
-//           ]
-//         }
-//       ]}]}}}
-//     );
+  httpMock.expectJSONP("http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK").respond(
+      {"MRData": {"StandingsTable": {"StandingsLists" : [{"DriverStandings":[
+        {
+          "Driver": {
+              "givenName": 'Sebastian',
+              "familyName": 'Vettel'
+          },
+          "points": "397",
+          "nationality": "German",
+          "Constructors": [
+              {"name": "Red Bull"}
+          ]
+        },
+        {
+          "Driver": {
+              "givenName": 'Fernando',
+              "familyName": 'Alonso'
+          },
+          "points": "242",
+          "nationality": "Spanish",
+          "Constructors": [
+              {"name": "Ferrari"}
+          ]
+        },
+        {
+          "Driver": {
+              "givenName": 'Mark',
+              "familyName": 'Webber'
+          },
+          "points": "199",
+          "nationality": "Australian",
+          "Constructors": [
+              {"name": "Red Bull"}
+          ]
+        }
+      ]}]}}}
+    );
 
-// var driversCtrl = $controller("driversCtrl", {
-//   $scope: scope
-// });
+driversCtrl = $controller("driversCtrl", {
+  $scope: scope
+});
 
 
-// httpMock.flush();
-// }));
+httpMock.flush();
+}));
 
-// it('should return a list with three drivers', function (){
-//   expect(scope.driversList.length).toBe(3);
-// });
+it('should return a list with three drivers', function (){
+  expect(scope.driversList.length).toBe(3);
+});
 
-// it('should retrieve the family names of the drivers', function (){
-//   expect(scope.driversList[0].Driver.familyName).toBe('Vettel');
-//   expect(scope.driversList[1].Driver.familyName).toBe('Alonso');
-//   expect(scope.driversList[2].Driver.familyName).toBe('Webber');
+it('should retrieve the family names of the drivers', function (){
+  expect(scope.driversList[0].Driver.familyName).toBe('Vettel');
+  expect(scope.driversList[1].Driver.familyName).toBe('Alonso');
+  expect(scope.driversList[2].Driver.familyName).toBe('Webber');
 
-// });
+});
 
 });
 
